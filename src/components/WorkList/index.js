7f11cdd6f50fb9cc8fe206af1000a183fca6f8db
LeeJuEun <LeeJuEun@users.noreply.github.com>
2019-04-25 03:18:13 +0900

    Update index.js

--- src/components/WorkList/index.js
+++ src/components/WorkList/index.js
@@ -123,26 +123,11 @@ class Work extends Component {
   }
 }
 
-/*
-1. App 컴포넌트에서 onScroll 이벤트 리스닝
-2. App 컴포넌트의 자식들을 ref 로 가져 오기 
-3. App 에서 WorkList.onScroll() 호출하기
-4-1. WorkList.onScroll 에서 worklist 의 Top 을 받아서 Work 1번 열기 - top을 받으려면 app에썼던 코드 또 써야해?
-4-2. WorkList.onScroll 에서 Work 1번 의 bottom 을 받아서 Work 2번 열기
-4-3. WorkList.onScroll 에서 Work 2번 의 bottom 을 받아서 Work 3번 열기
- 여는건 css로하믄되 ReactCSSTransitionGroup이런거 쓰면되나 맨위에한거??????그건 웅 css인뎅
- css로 할수는잇는데 계속 그거쓰는게 옳은건가 싶어서 웅냥ㅠㅠ 해볼게유 일단 집가서 해야긋다
-
-    // console.log("home.top", ReactDOM.findDOMNode(this).getBoundingClientRect().top); -> 워크리스트에서 해야되는거지?
-
-3. Worklist 의 각 work 를 차례차례 열기
-*/
 class WorkList extends Component {
   render() {
     return (
       <div id="worklist" className="worklist">
         <span id="work" className="sub_tit">Portfolio</span>
-        {/* 여기에 Ref 추가하기 */}
         {this.props.works && this.props.works.map((work, i) => (
           <Work i={i+1} key={i} work={work} ref={input => this[`work${i}`] = input} />)
         )}
